0a6eeed9b55b4c34888f7bc94e4d90cbfec54c6b
Lukas Schlüter <lukas.schlueter@gmail.com>
2023-12-20 00:13:48 +0100

    fix github template issues

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,8 @@ async fn main() {
     use axum::{routing::post, Router};
     use leptos::*;
     use leptos_axum::{generate_route_list, LeptosRoutes};
-    use {{crate_name}}::app::*;
-    use {{crate_name}}::fileserv::file_and_error_handler;
+    use leptos_transition_fallback_bug::app::*;
+    use leptos_transition_fallback_bug::fileserv::file_and_error_handler;
 
     simple_logger::init_with_level(log::Level::Debug).expect("couldn't initialize logging");
 
